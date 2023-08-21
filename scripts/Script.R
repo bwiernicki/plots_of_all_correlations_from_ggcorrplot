e0e89581d2163d9c13c2676f712db809ecd1cb1c
bwiernicki <b.wiernicki@gmail.com>
2023-08-21 07:12:32 +0200

    Initial commit.

--- scripts/Script.R
+++ scripts/Script.R
@@ -1,46 +1,41 @@
-
-
-
 # Loading the libraries ----
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
 library(ggpubr)
-library(gridExtra)
 library(ggcorrplot)
 
+# Load the data
 data("mtcars")
 
+# Select only numeric columns. In this example some of the columns were removed from the analysis
+mtcars <- mtcars %>%  select_if(is.numeric) %>% 
+  select(-c(cyl, vs, am, gear, carb))
 
-dim(mtcars)
-colnames(mtcars
-         )
-str(mtcars)
-
+# Create correlations matrix and the p.value matrix using the ggcorrplot package
 correlations <- cor(mtcars)
 p.values.correlations <- cor_pmat(mtcars)
 
-
-  ggcorrplot(correlations, 
+# Create the correlogram
+correlogram <- 
+ggcorrplot(correlations, 
              method='square', 
-             # hc.order = TRUE, 
              outline.col = "white",
              type='upper',
              sig.level = 0.05,
              insig = 'blank',
              colors = c("dodgerblue3", "white", "darkorange"),
              p.mat = p.values.correlations) +
-  #my_theme() +
   theme(axis.text.x = element_text(angle=45, hjust=1)) +
   labs(x=NULL, y=NULL) 
 
+correlogram
 # Correlation plots ---- 
 
-
-mtcars <- mtcars %>%  select_if(is.numeric) 
-
+# Change the p.value matrix into data frame
 p.values.correlations <- as.data.frame(p.values.correlations)  
 
+# Create a long dataframe with the p.values 
 p.values.correlations.sel <-
   p.values.correlations %>% 
   mutate(another = rownames(.)) %>% 
@@ -48,13 +43,14 @@ p.values.correlations.sel <-
                values_to = 'pvalue',
                names_to = 'first') %>% 
   filter(pvalue !=0) %>% 
-  filter(pvalue <=.05) %>% 
+  filter(pvalue <=.05) %>% # Filter only p values <=0.05
   mutate(combined = str_c(first, another, sep = '_')) %>% 
   mutate(sorted_words = sapply(strsplit(combined, "_"), 
                                function(x) paste(sort(x), collapse = "_"))) %>% 
   distinct(sorted_words, .keep_all = T) %>% 
   select(-combined, -sorted_words)
 
+# Function to create regression plots
 my_plot <- function(source, df) {
   x_var <- paste0(p.values.correlations.sel.item[1,2])
   y_var <- paste0(p.values.correlations.sel.item[1,1])
@@ -71,9 +67,10 @@ my_plot <- function(source, df) {
     labs(title = NULL,
          subtitle = y_var, y=NULL) +
     stat_cor(aes(label = paste(after_stat(rr.label), after_stat(p.label), sep = "*`,`~")),
-             label.y= Inf, label.x = Inf, vjust = 1, hjust = 1.1, size = 2.5)
+             label.y= Inf, label.x = Inf, vjust = 1, hjust = 1.1, size = 3.5)
 }
 
+# Creating a list of plots
 plots.corr <- list()
 for (i in (1:nrow(p.values.correlations.sel))){
   p.values.correlations.sel.item <- p.values.correlations.sel[i,]
@@ -85,44 +82,20 @@ for (i in (1:nrow(p.values.correlations.sel))){
               unique(p.values.correlations.sel.item$first))]] <- plot_i
 }
 
+# Saving the plots to the disc.
+plot.names <- names(plots.corr)
+lapply(seq_along(plots.corr), function(x) {
+  plot <- plots.corr[[x]]
+  name <- plot.names[x]
+  
+  ggsave(plot = plot, 
+         filename = paste0('./Plots/Correlations/',name, '.png'),
+         width = 80,
+         height = 80, units = 'mm')
+})
 
-lapply(plots.corr, function(x) ggsave(plot = x, filename = paste0('./Plots/Correlations/',
-                                                                  names(plots.corr),
-                                                                  '.png')))
+# Arranging the plots
+regressions <- 
+ggarrange(plotlist = plots.corr)
 
-
-for (i in (1:nrow(p.mat.sel))){
-  p.mat.sel5 <- p.mat.sel[i,]
-  my_plot(source = p.mat.sel5, df = data)
-  ggsave(plot = last_plot(),
-         filename = paste0('./Plots/Correlations/',
-                           unique(p.mat.sel5$another), '_',
-                           unique(p.mat.sel5$first),
-                           '.png'),
-         width= 90,
-         height = 60,
-         unit='mm')
-}
-
-
-# corrplot <<- 
-#   ggcorrplot(data3, 
-#              method='square', 
-#              # hc.order = TRUE, 
-#              outline.col = "white",
-#              type='upper',
-#              sig.level = 0.05,
-#              insig = 'blank',
-#              colors = c("dodgerblue3", "white", "darkorange"),
-#              p.mat = p.mat) +
-#     #my_theme() +
-#     theme(axis.text.x = element_text(angle=45, hjust=1)) +
-#     labs(x=NULL, y=NULL) 
-# 
-# if (y == 'yes'){
-# 
-# ggsave(filename = paste0('./Plots/Correlations/', unique(data$Cells),'/',unique(data$Cells), '_correlogram.png'),
-#        width = 160, height = 160, units = 'mm')
-# }
-# }
-
+regressions
